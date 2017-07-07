9143273b986e7d86c5c2adddf8a61ff733c3bc13
Bradley Ramuans <bradleyramunas@gmail.com>
2017-07-06 23:10:14 -0400

    Initial commit

---
+++ app/src/main/java/com/bradleyramunas/billionairebull/GameSelectActivity.java
@@ -0,0 +1,18 @@
+package com.bradleyramunas.billionairebull;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GameSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_select);
+    }
+
+    public void onCreateNewGameSelect(View view){
+
+    }
+}
